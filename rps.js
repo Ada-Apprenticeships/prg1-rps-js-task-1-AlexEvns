7aa9395c5f1175c94855ab49b20d1a3218b35b94
Alex Evans <alex.evans@ada.ac.uk>
2024-09-25 10:20:12 +0000

    Basic fully tested program

--- rps.js
+++ rps.js
@@ -1,7 +1,49 @@
 function rockPaperScissors(player1, player2) {
-
+  if(player1 === player2) {
+    return "draw";
+  } else if (player1 === "rock" && player2 === "scissors") {
+    return "player1";
+  } else if (player1 === "rock" && player2 === "lizard") {
+    return "player1";
+  } else if (player1 === "paper" && player2 === "rock") {
+    return "player1";
+  } else if (player1 === "paper" && player2 === "spock") {
+    return "player1";
+  } else if (player1 === "scissors" && player2 === "paper") {
+    return "player1";
+  } else if (player1 === "scissors" && player2 === "lizard") {
+    return "player1";
+  } else if (player1 === "lizard" && player2 === "paper") {
+    return "player1";
+  } else if (player1 === "lizard" && player2 === "spock") {
+    return "player1";
+  } else if (player1 === "spock" && player2 === "rock") {
+    return "player1";
+  } else if (player1 === "spock" && player2 === "scissors") {
+    return "player1";
+  } 
+    else if (player2 === "rock" && player1 === "scissors") {
+    return "player2";
+  } else if (player2 === "rock" && player1 === "lizard") {
+    return "player2";
+  } else if (player2 === "paper" && player1 === "rock") {
+    return "player2";
+  } else if (player2 === "paper" && player1 === "spock") {
+    return "player2";
+  } else if (player2 === "scissors" && player1 === "paper") {
+    return "player2";
+  } else if (player2 === "scissors" && player1 === "lizard") {
+    return "player2";
+  } else if (player2 === "lizard" && player1 === "paper") {
+    return "player2";
+  } else if (player2 === "lizard" && player1 === "spock") {
+    return "player2";
+  } else if (player2 === "spock" && player1 === "rock") {
+    return "player2";
+  } else if (player2 === "spock" && player1 === "scissors") {
+    return "player2";
+  } 
   
-   
 }
 
 
